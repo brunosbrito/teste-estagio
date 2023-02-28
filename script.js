d687af4c41b4b43a8d046a77e63e5456b8a1915a
brunosbrito <brunobrito728@gmail.com>
2023-02-27 22:39:44 -0300

    [refactor] refatora o código dos exercícios

--- script.js
+++ script.js
@@ -1,71 +1,76 @@
 // questão número 2
 
-let numero = 5
+const ENTRADA_1 = 5
 
-let num1 = 0;
+let num1 = 1;
 let num2 = 1;
 let num3;
 
-while (num2 < numero) {
+while (num2 < ENTRADA_1) {
   num3 = num1 + num2;
   num1 = num2;
   num2 = num3;
 }
 
-if (num2 == numero) {
-  console.log(numero + " pertence à sequência de Fibonacci.");
+if (num2 === ENTRADA_1) {
+  console.log(ENTRADA_1 + " pertence à sequência de Fibonacci.");
 } else {
-  console.log(numero + " não pertence à sequência de Fibonacci.");
+  console.log(ENTRADA_1 + " não pertence à sequência de Fibonacci.");
 }
 
 
 //questão número 3
 
-const dados = require('./dados.json')
+const data = require('./dados.json')
 
-let arrayValor = dados.map(dados => dados.valor);
-let faturamentoDiario = arrayValor.filter((valor) => valor !== 0)
-console.log(faturamentoDiario)
-
-let menorFaturamento = faturamentoDiario[0];
-let maiorFaturamento = faturamentoDiario[0];
+const arrayValue = data.map(data => data.valor);
+const dailyBilling = arrayValue.filter(value => value !== 0)
 
 // Soma o faturamento mensal
-let totalFaturamento = 0;
-for (let i = 0; i < faturamentoDiario.length; i += 1) {
-  totalFaturamento += faturamentoDiario[i];
+let totalBilling = 0;
+
+const DAILY_BILLING_LENGTH = dailyBilling.length;
+
+for (let i = 0; i < DAILY_BILLING_LENGTH; i += 1) {
+  totalBilling += dailyBilling[i];
 }
 
 // calcula a media mensal
-let mediaMensal = totalFaturamento / faturamentoDiario.length;
-
-let diasAcimaDaMedia = 0;
+const monthlyMedia = totalBilling / dailyBilling.length;
 
 /* verifica o menor e o maior valor de faturamento ocorrido em um dia e o 
   número de dias no mes em que o faturamento diario foi maior */ 
-for (let i = 0; i < faturamentoDiario.length; i++) {
 
-  if (faturamentoDiario[i] < menorFaturamento) {
-    menorFaturamento = faturamentoDiario[i];
+let lowerBilling = dailyBilling[0];
+let higherBilling = dailyBilling[0];
+
+let aboveAverageDays = 0;
+
+for (let i = 0; i < DAILY_BILLING_LENGTH; i += 1) {
+
+  const BILLING = dailyBilling[i]
+
+  if (BILLING < lowerBilling) {
+    lowerBilling = dailyBilling[i];
   }
-  if (faturamentoDiario[i] > maiorFaturamento) {
-    maiorFaturamento = faturamentoDiario[i];
+  if (BILLING > higherBilling) {
+    higherBilling = dailyBilling[i];
   }
  
-  if (faturamentoDiario[i] > mediaMensal) {
-    diasAcimaDaMedia++;
+  if (BILLING > monthlyMedia) {
+    aboveAverageDays += 1;
   }
 }
 
-console.log(`O menor valor de faturamento ocorrido em um dia do mês: ${menorFaturamento}`);
-console.log(`O maior valor de faturamento ocorrido em um dia do mês: ${maiorFaturamento}`);
-console.log(`Número de dias no mês em que o valor de faturamento diário foi superior à média mensal: ${diasAcimaDaMedia} `);
+console.log(`O menor valor de faturamento ocorrido em um dia do mês: ${lowerBilling}`);
+console.log(`O maior valor de faturamento ocorrido em um dia do mês: ${higherBilling}`);
+console.log(`Número de dias no mês em que o valor de faturamento diário foi superior à média mensal: ${aboveAverageDays} `);
 
 
 // questão 4
 
 // cria objeto com os estados e faturamentos
-const faturamentoPorEstado = {
+const BILLING_PER_STATE = {
   "SP": 67836.43,
   "RJ": 36678.66,
   "MG": 29229.88,
@@ -74,25 +79,34 @@ const faturamentoPorEstado = {
 };
 
 // calcula o valor total dos faturamentos mensais
-const valorTotalMensal = Object.values(faturamentoPorEstado).reduce((acc, curr) => acc + curr, 0);
+const VALUES_PER_STATE = Object.values(BILLING_PER_STATE);
+
+const TOTAL_VALUES = VALUES_PER_STATE.reduce((acc, curr) => acc + curr, 0);
 
 // calcula o porcentual de faturamento mensal de cada estado
-const percentuaisPorEstado = {};
-for (let estado in faturamentoPorEstado) {
-  percentuaisPorEstado[estado] = `${(faturamentoPorEstado[estado] / valorTotalMensal).toFixed(2) * 100}% ` ;
-  console.log()
+const PERCENTAGES_BY_STATE = {};
+
+for (let state in BILLING_PER_STATE) {
+
+  const STATE_BILLING = BILLING_PER_STATE[state];
+  const PERCENTAGES = (STATE_BILLING / TOTAL_VALUES).toFixed(2) * 100;
+
+  PERCENTAGES_BY_STATE[state] = ` ${PERCENTAGES}% `; 
 }
 
-console.log(percentuaisPorEstado);
+console.log(PERCENTAGES_BY_STATE);
 
 
 // questão 5
-function inverterString(str) {
-  let novaString = '';
-  for (let i = str.length - 1; i >= 0; i--) {
-    novaString += str[i];
+function reverseString(str) {
+
+  let newString = '';
+  const STRING_LENGTH = str.length;
+
+  for (let i = STRING_LENGTH - 1; i >= 0; i--) {
+    newString += str[i];
   }
-  return novaString;
+  return newString;
 }
 
-console.log(inverterString('Bruno'))
+console.log(reverseString('Bruno'))
